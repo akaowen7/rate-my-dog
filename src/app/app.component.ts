4547397a94f16e6149038bc191455c25b54f655e
Reid Tull <reid.a.tull@gmail.com>
2023-01-20 17:12:56 -0500

    got the dog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DogService } from './services/dog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'rate-my-dog';
+  dogImage : string = "";
+  
+  constructor(private dogService: DogService) { 
+    this.dogService.getDog().subscribe((dog: any) => {
+      this.dogImage = dog["message"];
+    });
+  }
+
 }
---
+++ src/app/services/dog.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DogService {
+
+  dogUrl = "https://dog.ceo/api/breeds/image/random"
+
+  constructor(private http: HttpClient) { }
+
+  getDog(): Observable<any> {
+    return this.http.get(this.dogUrl);
+  }
+}
