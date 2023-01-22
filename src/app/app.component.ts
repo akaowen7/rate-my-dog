f6d13df7b76596df7e03d8634f7ab72bc0d835e3
Reid Tull <reid.a.tull@gmail.com>
2023-01-21 20:56:23 -0500

    getting started on the history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 import { DogService } from './services/dog.service';
 import { NgbRatingConfig} from '@ng-bootstrap/ng-bootstrap';
+import { Rating } from './Rating';
+
+import { HistoryComponent } from './components/history/history.component';
+import { RatingComponent } from './components/rating/rating.component';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +19,9 @@ export class AppComponent {
   title = 'rate-my-dog';
   dogImage : string = "";
   dogRate : number = 0;
+  ratings : Rating[] = [];
   
-  constructor(private dogService: DogService, config: NgbRatingConfig) { 
+  constructor(private dogService: DogService, config: NgbRatingConfig, public dialog: MatDialog) { 
     this.dogService.getDog().subscribe((dog: any) => {
       this.dogImage = dog["message"];
     });
@@ -27,8 +33,23 @@ export class AppComponent {
     console.log(this.dogRate);
     this.dogRate = 0;
     
+    this.ratings.push({id: this.ratings.length, rating: this.dogRate, dogURL: this.dogImage});
+
     this.dogService.getDog().subscribe((dog: any) => {
       this.dogImage = dog["message"];
+    });
+  }
+
+  toggleHistory() {
+    const dialogRef = this.dialog.open(HistoryComponent, {
+      width: '100vh',
+      height:  '80vh',
+      maxWidth: '100vh',
+      maxHeight: '80vh',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbRatingModule} from '@ng-bootstrap/ng-bootstrap';
+import { HistoryComponent } from './components/history/history.component';
+import { RatingComponent } from './components/rating/rating.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     NgbModule,
     NgbRatingModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
