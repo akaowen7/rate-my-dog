aa622334d831fbd625f26eaad6f878357924cb6b
Reid Tull <reid.a.tull@gmail.com>
2023-01-22 14:36:08 -0500

    wow look at this sorting!

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -12,8 +12,25 @@ export class HistoryComponent {
   ratings: Rating[] = [
     {rating: 2, dogURL: 'https://images.dog.ceo/breeds/frise-bichon/6.jpg'}
   ];
+
+  highestToLowest = true;
+  buttonText = "Sort by lowest";
+
   // https://material.angular.io/components/dialog/overview#sharing-data-with-the-dialog-component
   constructor(@Inject(MAT_DIALOG_DATA) public data: {ratings: Rating[]}) { 
+    this.ratings.sort((a, b) => {
+      return a.rating - b.rating;
+    });
+
     this.ratings = data.ratings;
+  }
+
+  toggleSort() {
+    this.highestToLowest = !this.highestToLowest;
+    this.buttonText = this.highestToLowest ? "Sort by highest" : "Sort by lowest";
+
+    this.ratings = this.ratings.sort((a, b) => {
+      return this.highestToLowest ? a.rating - b.rating : b.rating - a.rating;
+    });
   }
 }
